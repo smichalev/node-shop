8023d0d3bacd7cb814eb08f3b39806b404a00b2f
smichalev <smichalev95@gmail.com>
2020-05-17 22:22:59 +0400

    issue7
    - Install express-session
    - Add express-session in project
    - Add middleware express-session
    - Add config file project

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const port = 3333;
+const config = require('config');
 
 const app = express();
 
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: config.session,
+}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 require('routes')(app);
 
-app.listen(port, () => {
-	console.log(`Server start at port ${port}`);
+app.listen(config.port, () => {
+	console.log(`Server start at port ${config.port}`);
 });
